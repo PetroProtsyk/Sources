0900689cfa09b1d237b32d3e23b6c660ca3920bd
petro <piter.protsyk@gmail.com>
2013-01-30 21:11:41 +0100

    Find the min (initial)

---
+++ Facebook Hackerup 2013/Find the Min/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+  class Program
+  {
+    class Counter
+    {
+      public int Value;
+      public Counter() { Value = 1; }
+      public void Inc()
+      {
+        Value++;
+      }
+
+      public void Dec()
+      {
+        Value--;
+      }
+    }
+
+    static void Main(string[] args)
+    {
+      int nc = int.Parse(Console.ReadLine());
+
+      for (int i = 0; i < nc; i++)
+      {
+        string [] kn = Console.ReadLine().Split(' ');
+        string [] abcr = Console.ReadLine().Split(' ');
+
+        int n = int.Parse(kn[0]);
+        int k = int.Parse(kn[1]);
+
+        int a = int.Parse(abcr[0]);
+        int b = int.Parse(abcr[1]);
+        int c = int.Parse(abcr[2]);
+        int r = int.Parse(abcr[3]);
+
+        List<int> v = new List<int>();
+        SortedDictionary<int, Counter> sd = new SortedDictionary<int, Counter>();
+
+        long m = a;
+        v.Add(a);
+        sd.Add(a, new Counter());
+
+        for (int j = 1; j < k; j++)
+        {
+          m = (b * m + c) % r;
+
+          v.Add((int)m);
+          Counter cv;
+          if (sd.TryGetValue((int)m, out cv))
+          {
+            cv.Inc();
+          }
+          else
+          {
+            sd.Add((int)m, new Counter());
+          }
+        }
+
+        int l = 0;
+        int p = 0;
+        while (true)
+        {
+          p = FindNext(sd);
+
+          if (p == k && (sd.First().Key == 0) && (sd.Last().Key == k-1))
+          {
+            // We have all values in [0..k-1], there is no need to search for other values
+            int f;
+            for (int o = 0; o < (n - k) % (k * (k+1)); o++, l++)
+            {
+              l = l % v.Count;
+              f = v[l];
+              v[l] = p;
+              p = f;
+            }
+
+            if (l > 0)
+              p = v[l - 1];
+            else
+              p = v[v.Count - 1];
+
+            break;
+          }
+
+          if (l < v.Count)
+          {
+            l++;
+          }
+          else
+          {
+            l = 1;
+          }
+
+          Counter cv;
+          if (sd.TryGetValue(v[l - 1], out cv))
+          {
+            cv.Dec();
+            if (cv.Value == 0)
+            {
+              sd.Remove(v[l - 1]);
+            }
+          }
+
+          if (sd.TryGetValue((int)p, out cv))
+          {
+            cv.Inc();
+          }
+          else
+          {
+            sd.Add((int)p, new Counter());
+          }
+
+          v[l - 1] = p;
+
+          n--;
+
+          if (n == k)
+          {
+            break;
+          }
+        }
+
+
+        /// Naive enumeration
+        /*
+        int p = 0;
+        int l = 0;
+        while (true)
+        {
+          if (!v.Contains(p))
+          {
+
+            if (l < v.Count)
+            {
+              v[l] = p;
+              l++;
+            }
+            else
+            {
+              v[0] = p;
+              l = 1;
+            }
+
+            n--;
+
+            if (n == k)
+            {
+              break;
+            }
+
+            p = 0;
+          }
+          else
+          {
+            p++;
+          }
+        }*/
+
+        Console.WriteLine("Case #{0}: {1}", i + 1, p);
+      }
+    }
+
+    private static int FindNext(SortedDictionary<int, Counter> sd)
+    {
+      if (sd.Count == 0)
+        return 0;
+
+      int prev = 0;
+      int current = 0;
+      foreach (int sv in sd.Keys)
+      {
+        current = sv;
+        if (prev < current)
+        {
+          return prev;
+        }
+        else
+        {
+          prev = current + 1;
+        }
+      }
+      return current + 1;
+    }
+
+    private static bool Contains(List<long> v, int p, int k)
+    {
+      for (int i = 0; i < k; i++)
+      {
+        if (v[v.Count - k + i] == p)
+          return true;
+      }
+      return false;
+    }
+  }
+}
