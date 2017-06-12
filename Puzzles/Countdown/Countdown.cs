5b99731ba52386ce533dab33b95f2a4ba9eea8c0
petro <piter.protsyk@gmail.com>
2017-06-12 17:23:41 +0200

    Countdown

---
+++ Puzzles/Countdown/Countdown.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int target = int.Parse(Console.ReadLine());
+            foreach(var expression in GetAllExpressions(numbers, target))
+            {
+                Console.WriteLine($"{expression} = {target}");
+            }
+        }
+
+        private static IEnumerable<string> GetAllExpressions(int[] values, int target)
+        {
+            for (int i = 0; i < values.Length; ++i)
+            {
+                foreach (var expression in GetAllExpressionsOfSize(i, values, new bool[values.Length]))
+                {
+                    if (expression.Item2 == target)
+                    {
+                        yield return expression.Item1;
+                    }
+                }
+            }
+        }
+
+        private static IEnumerable<Tuple<string, int>> GetAllExpressionsOfSize(int size, int[] values, bool[] used)
+        {
+            if (size == 0)
+            {
+                for (int j = 0; j < used.Length; j++)
+                {
+                    if (used[j])
+                    {
+                        continue;
+                    }
+
+                    used[j] = true;
+                    yield return Tuple.Create(values[j].ToString(), values[j]);
+                    used[j] = false;
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                foreach (var nodeLeft in GetAllExpressionsOfSize(i, values, used))
+                {
+                    foreach (var nodeRight in GetAllExpressionsOfSize(size - 1 - i, values, used))
+                    {
+                        if (nodeLeft.Item2 <= nodeRight.Item2)
+                        {
+                            yield return Tuple.Create($"({nodeLeft.Item1}+{nodeRight.Item1})", nodeLeft.Item2 + nodeRight.Item2);
+
+                            if (nodeLeft.Item2 != 1)
+                            {
+                                yield return Tuple.Create($"({nodeLeft.Item1}*{nodeRight.Item1})", nodeLeft.Item2 * nodeRight.Item2);
+                            }
+                        }
+
+                        if (nodeLeft.Item2 - nodeRight.Item2 > 0)
+                        {
+                            yield return Tuple.Create($"({nodeLeft.Item1}-{nodeRight.Item1})", nodeLeft.Item2 - nodeRight.Item2);
+                        }
+
+                        if (nodeRight.Item2 != 1 && nodeLeft.Item2 % nodeRight.Item2 == 0)
+                        {
+                            yield return Tuple.Create($"({nodeLeft.Item1}/{nodeRight.Item1})", nodeLeft.Item2 / nodeRight.Item2);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Puzzles/Countdown/Task.md
@@ -0,0 +1,8 @@
+The task is described in this article: http://www.cs.nott.ac.uk/~pszgmh/countdown.pdf
+
+*Countdown Problem*
+Given a sequence of numbers and a target number, attempt to construct an expression whose value is the target, by combining one or more numbers from the sequence using addition, subtraction, multiplication, division and parentheses.
+Each number in the sequence can only be used at most once in the expression, and all numbers involved, including intermediate values, must be positive natural numbers (1, 2, 3, ...). In particular, the use of negative numbers, zero, and proper fractions such as 2/3 is not permitted.
+
+For example, suppose we are given the sequence 1,3,7,10,25,50, and the target 765. Then one possible solution is given by the expression (1+50)*(25-10).
+For a set of 7 numbers [3,19,45,21,10,4,7] and a target number 100 all solutions have to be found within 7 seconds.
