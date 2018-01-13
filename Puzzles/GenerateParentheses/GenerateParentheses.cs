856ef64e1ed889d3e20fe25a4285733a4ddca882
Petro <petro@unknown.com>
2018-01-13 08:45:25 +0100

    Generate Parentheses

---
+++ Puzzles/GenerateParentheses/GenerateParentheses.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace GenerateParentheses
+{
+    /// <summary>
+    /// Generate all valid (properly opened and closed) combinations of n pairs of parentheses.
+    /// </summary>
+    class Program
+    {
+        static void Solve(int stillToOpen, int currentlyOpened, int position, char[] result)
+        {
+            if (position == result.Length)
+            {
+                Console.WriteLine(new string(result));
+                return;
+            }
+
+            if (stillToOpen > 0)
+            {
+                result[position] = '(';
+                Solve(stillToOpen - 1, currentlyOpened + 1, position + 1, result);
+            }
+
+            if (currentlyOpened > 0)
+            {
+                result[position] = ')';
+                Solve(stillToOpen, currentlyOpened - 1, position + 1, result);
+            }
+        }
+
+        static void Solve(int n)
+        {
+            Solve(n, 0, 0, new char[2*n]);
+        }
+
+        static void Main(string[] args)
+        {
+            Solve(3);
+        }
+    }
+}
