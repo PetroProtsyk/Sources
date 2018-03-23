133d464af004f724d93f357fddf8939ae01e4e0f
Petro <petro@unknown.com>
2018-03-23 19:59:03 +0100

    Wildcard pattern matching

---
+++ Algorithms/BruteForceWildcard.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Protsyk.Algorithms
+{
+    public static class BruteForceWildcard
+    {
+        public static bool Match(string pattern, string word)
+        {
+            return MatchRecursive(pattern, 0, word, 0);
+        }
+
+        private static bool MatchRecursive(string p, int startP, string a, int startA)
+        {
+            var ai = startA;
+            for (int i = startP; i < p.Length; ++i)
+            {
+                if (ai >= a.Length)
+                {
+                    // Word matched, skip trailing stars
+                    if (p[i] != '*')
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (p[i] == a[ai] || p[i] == '?')
+                    {
+                        ai++;
+                    }
+                    else if (p[i] == '*')
+                    {
+                        for (int j = ai; j < a.Length; ++j)
+                        {
+                            if (MatchRecursive(p, i + 1, a, j))
+                            {
+                                return true;
+                            }
+                        }
+                        return false;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Algorithms/DynamicProgrammingWildcard.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Protsyk.Algorithms
+{
+    public class DynamicProgrammingWildcard
+    {
+        public static bool Match(string pattern, string word)
+        {
+            var m = new bool[pattern.Length + 1, word.Length + 1];
+            m[0, 0] = true;
+            for (int i = 0; i < pattern.Length; ++i)
+            {
+                if (pattern[i] == '*')
+                {
+                    m[i + 1, 0] = m[i, 0];
+                }
+            }
+
+            for (int i = 0; i < pattern.Length; ++i)
+            {
+                for (int j = 0; j < word.Length; ++j)
+                {
+                    if (pattern[i] == '*')
+                    {
+                        m[i + 1, j + 1] = m[i, j] || m[i + 1, j] || m[i, j + 1];
+                    }
+                    else
+                    {
+                        m[i + 1, j + 1] = m[i, j] & (pattern[i] == word[j] || pattern[i] == '?');
+                    }
+                }
+            }
+
+            return m[pattern.Length, word.Length];
+        }
+    }
+}
